e9bee0a7bfbdb3b80a85e077bcaa55ceeca3e988
leonardo-sarda <leonardo_sarda@hotmail.com>
2025-02-24 18:01:43 -0300

    funçao do desafio

---
+++ desafio.php
@@ -0,0 +1,41 @@
+<?php
+function cakes($recipe, $ingredients){
+  // Desenvolver o código nesta function,
+  //$bolos = [];
+  $bolos = 999;
+  foreach ($recipe as $ingrediente => $quantidade) {
+    if (isset($ingredients[$ingrediente])) {
+        //return  0;
+        //$bolo = $ingred / $recipe[$inteiro];
+        //$bolos[] = $bolo
+      $boloIngrediente = intdiv($ingredients[$ingrediente], $quantidade);
+
+      if ($boloIngrediente < $bolos){
+        $bolos = $boloIngrediente;
+      }
+    }else{
+      return 0;
+    }
+
+    //if(isset($ingredients[$ingrediente])==0){
+    //  return 0;
+    //}
+  }
+  return $bolos;
+  
+}
+// Abaixo estão os casos de uso:
+var_dump(cakes(
+  ['flour' => 500, 'sugar' => 200, 'eggs' => 1], 
+  ['flour' => 1200, 'sugar' => 1200, 'eggs' => 5, 'milk' => 200]) 
+  === 2); 
+
+var_dump(cakes(
+  ['apples' => 3, 'flour' => 300, 'sugar' => 150, 'milk' => 100, 'oil' => 100], 
+  ['sugar' => 500, 'flour' => 2000, 'milk' => 2000]) 
+  === 0); 
+
+var_dump(cakes(
+  ['flour' => 500, 'sugar' => 200, 'eggs' => 1], 
+  ['flour' => 1500, 'sugar' => 1200, 'eggs' => 5, 'milk' => 200]) 
+  === 3); 
